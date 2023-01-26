2c8d65a0296aee1d07e81f57c7942c413a39471a
Julia Tran <julia.kim.tran@hotmail.com>
2023-01-25 21:15:12 -0500

    Thank you page

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navbarLinks = document.getElementsByClassName('navbar-links')[0];
-const buttons = document.querySelectorAll("[data-carousel-button]");
 const navigationHeight = document.querySelector(".navbar").offsetHeight;
 const logo = document.querySelector(".logo");
 const listen = document.querySelector(".btn");
 const links = document.querySelectorAll(".navbar-links li");
 const jellyfish = document.getElementsByClassName("cover-art")[0];
+const buttons = document.querySelectorAll("[data-carousel-button]")
+
 
 
 document.documentElement.style.setProperty('--scroll-padding', `${navigationHeight}px`);
@@ -36,20 +37,22 @@ jellyfish.addEventListener("click", () => {
   let url ='https://songwhip.com/tyyy2/jellyfish';
   window.open(url, "_blank").focus();
 });
-// Carousel
-// buttons.forEach(button => {
-//   button.addEventListener("click", () => {
-//     const offset = button.dataset.carouselButton === "next" ? 1 : -1
-//     const slides = button
-//       .closest("[data-carousel]")
-//       .querySelector("[data-slides]")
 
-//     const activeSlide = slides.querySelector("[data-active]")
-//     let newIndex = [...slides.children].indexOf(activeSlide) + offset
-//     if (newIndex < 0) newIndex = slides.children.length - 1
-//     if (newIndex >= slides.children.length) newIndex = 0
+//Carousal
 
-//     slides.children[newIndex].dataset.active = true
-//     delete activeSlide.dataset.active
-//   })
-// })
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]")
+
+    const activeSlide = slides.querySelector("[data-active]")
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    if (newIndex < 0) newIndex = slides.children.length - 1
+    if (newIndex >= slides.children.length) newIndex = 0
+
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.dataset.active
+  })
+})
